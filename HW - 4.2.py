69940f6bdcd57a57ffe7a726a6dd68d570c2352c
John-Moisha <pkgames.dp@gmail.com>
2020-09-27 13:32:21 +0300

    HW to folder

---
+++ HW - 4.2.py
@@ -0,0 +1,51 @@
+# Задания:
+# 1) У вас есть список my_list с значениями типа int.
+# Распечатать те значения, которые больше 100.
+# Задание выполнить с помощью цикла for.
+##########################
+# my_list1 = [100, 1, 5, 200, 13, 225, 101, 55, 303, 555]
+# result1 = []
+# for value in my_list1:
+#     value = int(value)
+#     if value > 100:
+#         my_list1 = result1.append(int(value))
+# print(result1)
+
+# my_list1 = [100, 1, 5, 200, 13, 225, 101, 55, 303, 555]
+# result1 = []
+# for value in my_list1:
+#      if int(value) > 100:
+#          print(value)
+
+
+# 2) У вас есть список my_list с значениями типа int, и пустой список my_results.
+# Добавить в my_results те значения, которые больше 100.
+# Распечатать список my_results.
+# Задание выполнить с помощью цикла for.
+##########################
+# my_list2 = [100, 1, 5, 200, 13, 225, 101, 55, 303, 555]
+# my_results2 = []
+# for value in my_list2:
+#     value = int(value)
+#     if value > 100:
+#         my_list2 = my_results2.append(int(value))
+# print(my_results2)
+
+# У вас есть список my_list с значениями типа int.
+# Если в my_list количество элементов меньше 2, то в конец добавить значение 0.
+# Если количество элементов больше или равно 2, то добавить сумму последних двух элементов.
+# Количество элементов в списке можно получить с помощью функции len(my_list)
+# my_list3 = [100, 1, 3]
+# my_list3 = type(int)
+# my_results3 = []
+# my_sum = my_list3[-1] + my_list3[-2]
+# for value in my_list3:
+#     value = int(value)
+#     if len(my_list3) < 2:
+#         my_list3 = my_list3.append('0')
+#     if len(my_list3) > 2:
+#         my_list3 = my_list3.append(my_sum)
+# print(my_results3)
+
+
+
